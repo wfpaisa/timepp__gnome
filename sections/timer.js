25b1f0897cfea3ed20349afbda61255ac0191aea
Felipe Uribe <wfpaisa@gmail.com>
2020-04-25 15:12:01 -0500

    It's necessary a title

--- sections/timer.js
+++ sections/timer.js
@@ -468,7 +468,7 @@ var SectionMain = class SectionMain extends ME.imports.sections.section_base.Sec
                 this.notif_source.destroyNonResidentNotifications();
             }
 
-            this.notif_source = new MessageTray.Source();
+            this.notif_source = new MessageTray.Source("timer");
             Main.messageTray.add(this.notif_source);
             this.notif_source.connect('destroy', () => this.sound_player.stop());
 
